92bf328cd0568f3c4209b40519f7c4526f25cc42
Mark Lamson <lamson_mark@yahoo.com>
2015-03-17 12:36:25 -0400

    added google map api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -58,75 +58,10 @@ angular.module('DayMap', ['ngRoute', 'ngCookies', 'uiGmapgoogle-maps',])
 
 
       });
-
-     //   $rootScope.$on('$routeChangeSuccess', function () {
-
-     //   	var location = $location.path();
-
-     //   	console.log(location, 'in routeChangeSuccess');
-
-     //   	if(location === '/home') {
-     //   		MapFactory.render();
-     //   	}
-       
-     // });
-
      
    }
-
-       
-
+   
   ]);
-
-
-
-    
-
-
-
-
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// var circle = L.circle([51.508, -0.11], 500, {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5
-// }).addTo(map);
-
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(map);
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-// var popup = L.popup()
-//     .setLatLng([43.093697, -78.876879])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
-
-//     var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
-
-
-// //L.mapbox.accessToken = 'pk.eyJ1IjoibWxhbXNvbiIsImEiOiJrR1lxejlZIn0.jNQ5v-SlOw7pzRjDgVRVhQ';
-
-
-// L.tileLayer('http://{s}.tiles.mapbox.com/v3/mlamson.lc66lkk4/{z}/{x}/{y}.png', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18
-// }).addTo(map);
 
 }());
 
--- app/scripts/maps/map.controller.js
+++ app/scripts/maps/map.controller.js
@@ -40,24 +40,54 @@
         $scope.trackMarkers = [];
         ////object to hold current marker
         $scope.marker = {};
+        ////variable needed to set id so idKey will have vaule.
         var i = 0;
+        //////get array of tracks from day
         d.tracks.map( function (x) {
-
+          ////get array of track from tracks
           x.track.map( function (y) {
-
+            ////set marker value
             $scope.marker = {id: i, coords: {latitude: y.latitude, longitude: y.longitude}};
+            //increment id var
             i++;
+            ////push marker into trackMarkers model which is an array
             $scope.trackMarkers.push($scope.marker);
-
-
           });
         
         });
 
-       
-console.log($scope.trackMarkers);
+        console.log($scope.trackMarkers[0].coords, 'tup');
+        $scope.tempCenter = $scope.trackMarkers[0].coords;
+        $scope.map = {center: $scope.tempCenter};
+        console.log($scope.map);
+
+        $scope.map = {
+          center: {
+            latitude: 0,//$scope.trackMarkers[0].coords.latitude,
+            longitude: 0//$scope.trackMarkers[0].coords.longitude
+          },
+          zoom: 1,
+          bounds: {}
+        };
+
+      //   $scope.markerClicked = $scope.trackMarkers[5];
+      //   console.log($scope.markerClicked.coords);
+      // var latLng = $scope.markerClicked.getPosition(); 
        
       };
+
+         // Creates the polyline object
+          var polyline = new google.maps.Polyline({
+            map: map,
+            path: path,
+            strokeColor: '#0000FF',
+            strokeOpacity: 0.7,
+            strokeWeight: 1
+          });
+
+
+      // returns LatLng object
+// map.setCenter(latLng); // setCenter takes a LatLng object
 
 /////////////////////////////////////////////////////////////////////
 
